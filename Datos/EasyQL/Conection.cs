6f777c586be576e130869180b39168d24635e309
EverthSarantes <everthleonardosaran@gmail.com>
2023-06-05 15:03:16 -0600

    Se ha añadido la libreria de datos

---
+++ Datos/EasyQL/Conection.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+
+namespace EasyQL
+{
+    public class Conection
+    {
+        private static string password;
+        private static SqlConnection con;
+        private static string server;
+        private static string dataBase;
+        private static string user;
+
+        public static string Server { get => server; set => server = value; }
+        public static string DataBase { get => dataBase; set => dataBase = value; }
+        public static string User { get => user; set => user = value; }
+        public static string Password { get => password; set => password = value; }
+        public static SqlConnection Con { get => con; set => con = value; }
+
+        public static string makeConnectionString()
+        {
+            if(User != "" && Password != "")
+            {
+                return $"Data Source={Server};Initial Catalog={DataBase};User ID={User};Password={Password};";
+            }
+            else if(User == "" && Password == "")
+            {
+                return $"Data Source={Server};Initial Catalog={DataBase};Integrated Security=true;";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static SqlConnection makeConnection()
+        {
+            Con = new SqlConnection(makeConnectionString());
+            return Con;
+        }
+    }
+}
---
+++ Datos/EasyQL/Estudiante.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+namespace EasyQL
+{
+    public class Estudiante : Model
+    {
+        public Estudiante(SqlConnection conection) : base(conection)
+        {
+            Fields.Add("nombres");
+            Fields.Add("apellidos");
+            Fields.Add("carnet");
+            Fields.Add("cedula");
+            Fields.Add("lugar_nac");
+            Fields.Add("fecha_nac");
+            Fields.Add("estado_civil");
+            Fields.Add("nacionalidad");
+            Fields.Add("telefono");
+            Fields.Add("direccion");
+            Fields.Add("departamento");
+            Fields.Add("municipio");
+           
+            Table = "Estudiante";
+        }
+    }
+}
